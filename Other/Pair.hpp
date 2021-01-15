9c1209ab04ac11b31ad540f501ff79c1073bb207
Mashiro <1262159823@qq.com>
2021-01-15 23:57:39 +0800

    Pair类型

---
+++ Other/Pair.hpp
@@ -0,0 +1,64 @@
+#pragma once
+
+template<typename Key_Ty,typename Value_Ty>
+struct pair
+{
+	Key_Ty key;
+	Value_Ty value;
+
+	pair(Key_Ty k,Value_Ty v):key(k),value(v)
+	{ }
+
+	pair()
+	{ }
+
+	void swap(pair<Key_Ty , Value_Ty>& obj)
+	{
+		auto temp_key = key;
+		auto temp_value = value;
+
+		key = obj.key;
+		value = obj.value;
+
+		obj.key = temp_key;
+		obj.value = temp_value;
+	}
+
+	void get(const pair<Key_Ty , Value_Ty> obj)
+	{
+		key = obj.key;
+		value = obj.value;
+	}
+
+	bool operator==(const pair<Key_Ty , Value_Ty> obj)
+	{
+		return key == obj.key;
+	}
+
+	bool operator>=(const pair<Key_Ty , Value_Ty> obj)
+	{
+		return key >= obj.key;
+	}
+
+	bool operator<=(const pair<Key_Ty , Value_Ty> obj)
+	{
+		return key <= obj.key;
+	}
+
+	bool operator>(const pair<Key_Ty , Value_Ty> obj)
+	{
+		return key > obj.key;
+	}
+
+	bool operator<(const pair<Key_Ty , Value_Ty> obj)
+	{
+		return key < obj.key;
+	}
+};
+
+
+template<typename key,typename value>
+inline pair<key , value> make_pair(const key &k , const value &v)
+{
+	return pair<key , value>(k , v);
+}
