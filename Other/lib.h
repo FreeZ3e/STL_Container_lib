e1ccdd9951350fc6ad710a1f6045695aa84847c5
Mashiro <1262159823@qq.com>
2021-02-19 19:24:56 +0800

    头文件

---
+++ Other/lib.h
@@ -0,0 +1,22 @@
+/*lib.h -included all files of container library
+*/
+#pragma once
+
+#include"Container_Array.hpp"
+#include"Container_Vector.hpp"
+#include"Container_Deque.hpp"
+#include"Container_List.hpp"
+#include"Container_Set.hpp"
+#include"Container_Multiset.hpp"
+#include"Container_Map.hpp"
+#include"Container_Multimap.hpp"
+#include"RB_Tree.hpp"
+#include"Pair.hpp"
+#include"HashTable.hpp"
+#include"Container_Unordered_Set.hpp"
+#include"Container_Unordered_Multiset.hpp"
+#include"Container_Unordered_Map.hpp"
+#include"Container_Unordered_Multimap.hpp"
+#include"Container_String.hpp"
+#include"Container_Stack.hpp"
+#include"Container_Queue.hpp"
